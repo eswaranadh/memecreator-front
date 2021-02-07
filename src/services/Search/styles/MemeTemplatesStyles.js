2729428006029042a9b862ea0b3af5c9505f4a38
sandeep7309 <sandeepaddanki11@gmail.com>
2021-02-07 23:16:02 +0530

    API Calls

--- src/services/Search/styles/MemeTemplatesStyles.js
+++ src/services/Search/styles/MemeTemplatesStyles.js
@@ -1,37 +1,38 @@
-import { StyleSheet } from 'react-native'
-import { windowWidth } from '../../../utils/Dimensions';
+import {StyleSheet} from 'react-native';
+import {windowWidth} from '../../../utils/Dimensions';
 
 export default StyleSheet.create({
-    container: {
-        flexDirection: 'column',
-        justifyContent: "space-between",
-        margin: 5,
-        
-      },
-      cardStyles: {
-        width: windowWidth/2.1,
-        marginRight:10,
-        marginTop:10,
-      },
-      headBar:{
-        display:'flex',
-        flexDirection:"row",
-        justifyContent:"space-between",    
-        alignContent:'stretch',
-      },
-      headContentOne:{
-          color:"#ffffff",
-          fontWeight:"bold",
-          fontSize: 19,
-      },
-      headContentTwo:{
-        color:"#ffffff",
-        fontWeight:"bold",
-        fontSize: 15,
-        alignSelf:'flex-end',
-        marginRight:10,
-        
-        
-        
-    },
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    margin: 5,
+  },
+  cardStyles: {
+    width: windowWidth / 2.1,
+    marginRight: 10,
+    marginTop: 10,
+  },
+  headBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'stretch',
+  },
+  headContentOne: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 19,
+  },
+  headContentTwo: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: 15,
+    alignSelf: 'flex-end',
+    marginRight: 10,
+  },
+  // loader: {
+  //   marginTop: 10,
+  //   alignItems: 'center',
+  //   color: '#ffffff',
+  // },
 });
